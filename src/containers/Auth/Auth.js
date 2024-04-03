b01aa38c63f39afd36b5feae4ca1909e7a6be70d
ishunter216 <michael.king@edenprogram.com>
2024-04-03 12:48:24 +0300

    Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import Button from '../../components/UI/Button/Button';
-import Input from '../../components/UI/Input/Input'
-import './Auth.css'
+import Input from '../../components/UI/Input/Input';
+import axios from "axios";
+import './Auth.css';
 
 
 const validateEmail = (email) => {
@@ -45,12 +46,40 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = () => {
+  loginHandler = async (e) => {
+    e.preventDefault();
 
+    const loginData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
+    
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBnNOaXBF2_fA713IrMrOiDPXPIijkDHfQ', loginData);
+
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  resisterHandler = () => {
+  registerHandler = async (e) => {
+    e.preventDefault();
 
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
+
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBnNOaXBF2_fA713IrMrOiDPXPIijkDHfQ', authData);
+
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   submitHandler = () => {
